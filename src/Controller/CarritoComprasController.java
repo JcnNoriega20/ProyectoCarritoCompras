eb277bd2797b68d6ed4b20062c9b95a70b554876
jcnoriega20 <jcnoriega20@gmail.com>
2023-06-12 17:52:19 -0500

    Configurado para que salga la ventana con los respectivos ajustes

--- src/Controller/CarritoComprasController.java
+++ src/Controller/CarritoComprasController.java
@@ -2,29 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package Clases;
+package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author wikicamus
  */
-public class FXMLDocumentController implements Initializable {
+public class CarritoComprasController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField txtUser;
+    @FXML
+    private TextField txtPassUser;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
